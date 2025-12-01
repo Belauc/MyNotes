cc8a8dc8499953ec3a3b91c0ae865a778c337695
Belauc <pochtijordan@gmail.com>
2025-12-01 21:51:34 +0300

    Initial Commit

---
+++ MyNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyNotes
+//
+//  Created by Belyantsev Sergey on 01.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
